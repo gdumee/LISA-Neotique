2c87e575410c61956fcbb0e0663c1b9dd6301310
Gilles Dumee <gilles.dumee@neotique.fr>
2014-07-25 13:36:07 +0200

    Debug translation validation, and answers

--- NeoDialog.py
+++ NeoDialog.py
@@ -39,7 +39,7 @@ class NeoContext():
     __steps = {'count': 0, 'first': None, 'last': None}
     __plugins = {}
     configuration_server = ConfigManagerSingleton.get().getConfiguration()
-        
+
     #-----------------------------------------------------------------------------
     def __init__(self, factory, client_uid):
         self.factory = factory
@@ -73,7 +73,7 @@ class NeoContext():
                     # TODO cancel timers
                     print "Count : {}".format(getrefcount(NeoContext.__global_ctx['Vars'][v][t]['timer']))
                     NeoContext.__global_ctx['Vars'][v][t]['timer'] = None
-                    
+
                 del NeoContext.__global_ctx['Vars']
                 NeoContext.__global_ctx['Vars'] = None
         for v in NeoContext.__global_ctx:
@@ -88,14 +88,14 @@ class NeoContext():
         if self._process_answer(jsonInput) == True:
             # The answer was processed
             return
-        
+
         # In no plugin is associated to the intent
         if module_name is None or function_name is None:
             # Add an error step
             step = self._create_step()
             step['type'] = "Error no plugin"
             step['in_json'] = jsonInput
-            
+
             # Return an error to client
             jsonData = {'type': 'Error', 'message': _("no_plugin")}
             self.factory.sendToClients(client_uids = [self.client['uid']], jsonData = jsonData)
@@ -107,12 +107,12 @@ class NeoContext():
             step = self._create_step()
             step['type'] = "error confidence"
             step['in_json'] = jsonInput
-            
+
             # Return an error to client
             jsonData = {'type': 'Error', 'message': _("Confidence too low")}
             self.factory.sendToClients(client_uids = [self.client['uid']], jsonData = jsonData)
             return
-            
+
         # Get initialized plugin
         plugin = None
         plugin_uid = None
@@ -136,20 +136,20 @@ class NeoContext():
                     plugin['instance'].uid = plugin_uid
                 plugin['steps'] = {'count': 0, 'first': None, 'last': None}
                 NeoContext.__plugins[plugin_uid] = plugin
-                
+
             except:
                 # Add an error step
                 step = self._create_step()
                 step['type'] = "error create plugin"
                 step['module_name'] = module_name
                 step['in_json'] = jsonInput
-                
+
                 # Return an error to client
                 jsonData = {'type': 'Error', 'message': _("Error initializing plugin")}
                 self.factory.sendToClients(client_uids = [self.client['uid']], jsonData = jsonData)
-                
+
                 return
-                
+
         # Get method to call
         methodToCall = None
         try:
@@ -161,13 +161,13 @@ class NeoContext():
             step['module_name'] = module_name
             step['function_name'] = function_name
             step['in_json'] = jsonInput
-            
+
             # Return an error to client
             jsonData = {'type': 'Error', 'message': _("Error plugin doesn't have this function")}
             self.factory.sendToClients(client_uids = [self.client['uid']], jsonData = jsonData)
-            
+
             return
-            
+
         # Save step in context
         step = self._create_step(plugin_uid = plugin_uid)
         step['type'] = "Plugin call"
@@ -176,7 +176,7 @@ class NeoContext():
         # Call plugin method
         jsonInput['context'] = self
         jsonOutput = methodToCall(jsonInput)
-        
+
         # Old Lisa plugin output
         if jsonOutput is not None:
             self.speakToClient(plugin_uid = plugin_uid, text = jsonOutput['body'])
@@ -185,13 +185,13 @@ class NeoContext():
     def speakToClient(self, plugin_uid, text, client_uids = [], zone_uids = []):
         """
         Speak to the client
-        
+
         plugin_uid : uid of the plugin calling the API
         text : speech for user
         context : current dialog context, can be None for global notification to user (associated with no context)
         client_uids : optional list of destination clients, use clients uids
         zone_uids : optional list of destination zones, use zone uids
-        
+
         To answer a client, do net set client_uids and zone_uids
         To send to everyone : client_uids = ['all'] or zone_uids = ['all']
         """
@@ -214,16 +214,16 @@ class NeoContext():
     def askClient(self, plugin_uid, text, answer_cbk, wit_context = {}, client_uids = [], zone_uids = []):
         """
         Ask a question, and wait for an answer
-        
+
         plugin_uid : uid of the plugin calling the API
         text : question for user
         client_uids : optional list of destination clients, use clients uids
         zone_uids : optional list of destination zones, use zone uids
         answer_cbk : function called on answer
-        
+
         To answer a client, do net set client_uids and zone_uids
         To send to everyone : client_uids = ['all'] or zone_uids = ['all']
-        
+
         The callback prototype is : def answer_cbk(self, context, jsonAnswer)
             context : identical to context given here
             jsonAnswer : json received from the client (!!may have no intent!!), None when no answer is received after a timeout
@@ -250,7 +250,7 @@ class NeoContext():
         step['answer_cbk'] = answer_cbk
         step['wait_timer'] = NeoTimer(duration_s = 20, user_cbk = self._timer_cbk, user_param = 0)
         self.wait_step = step
-        
+
         # Release access
         NeoContext.__lock.release()
 
@@ -261,18 +261,18 @@ class NeoContext():
         jsonData['message'] = text
         jsonData['wit_context'] = wit_context
         self.factory.sendToClients(client_uids = client_uids, zone_uids = zone_uids, jsonData = jsonData)
-        
+
     #-----------------------------------------------------------------------------
     def globalSpeakToClient(self, text, client_uids = [], zone_uids = []):
         """
-        
+
         """
         TODO
 
     #-----------------------------------------------------------------------------
     def globalAskToClient(self, text, answer_cbk, client_uids = [], zone_uids = []):
         """
-        
+
         """
         TODO
 
@@ -283,7 +283,7 @@ class NeoContext():
         """
         # No answer timeout
         self._process_answer()
-        
+
     #-----------------------------------------------------------------------------
     def _process_answer(self, jsonAnswer = None):
         # Lock access
@@ -294,7 +294,7 @@ class NeoContext():
             # Release access
             NeoContext.__lock.release()
             return False
-        
+
         # Keep step locally
         step = self.wait_step
         self.wait_step = None
@@ -308,7 +308,7 @@ class NeoContext():
         new_step['type'] = "Answer"
         new_step['question_step'] = step['uid']
         step['answer_step'] = new_step
-        
+
         # Release access
         NeoContext.__lock.release()
 
@@ -318,6 +318,8 @@ class NeoContext():
             jsonAnswer['context'] = self
 
         # Callback caller without answer
+        if jsonAnswer is not None:
+            jsonAnswer['context'] = self
         step['answer_cbk'](context = self, jsonAnswer = jsonAnswer)
 
         # Clear step
@@ -334,7 +336,7 @@ class NeoContext():
     def log(self):
         # Lock access
         NeoContext.__lock.acquire()
-        
+
         uid = NeoContext.__steps['first']
         while uid is not None:
             print NeoContext.__history[uid]
@@ -347,7 +349,7 @@ class NeoContext():
     def _create_step(self, plugin_uid = None):
         # Lock access
         NeoContext.__lock.acquire()
-        
+
         # Create a step
         step_uid = str(uuid.uuid1())
         step = {'uid': step_uid, 'date': datetime.now(), 'previous': None, 'next': None, 'client_uid': self.client['uid'], 'client_previous': None, 'client_next': None, 'plugin_uid': plugin_uid, 'plugin_previous': None, 'plugin_next': None}
@@ -357,42 +359,42 @@ class NeoContext():
         NeoContext.__steps['count'] += 1
         if NeoContext.__steps['first'] is None:
             NeoContext.__steps['first'] = step_uid
-        
+
         # Link to last step
         if NeoContext.__steps['last'] is not None:
             NeoContext.__history[NeoContext.__steps['last']]['next'] = step_uid
             step['previous'] = NeoContext.__history[NeoContext.__steps['last']]['uid']
         NeoContext.__steps['last'] = step_uid
-            
+
         # First client step
         self._client_steps['count'] += 1
         if self._client_steps['first'] is None:
             self._client_steps['first'] = step_uid
-        
+
         # Link to client last step
         if self._client_steps['last'] is not None:
             NeoContext.__history[self._client_steps['last']]['client_next'] = step_uid
             step['client_previous'] = NeoContext.__history[self._client_steps['last']]['uid']
         self._client_steps['last'] = step_uid
-        
+
         # link to a plugin
         if plugin_uid is not None:
             # First plugin step
             NeoContext.__plugins[plugin_uid]['steps']['count'] += 1
             if NeoContext.__plugins[plugin_uid]['steps']['first'] is None:
                 NeoContext.__plugins[plugin_uid]['steps']['first'] = step_uid
-            
+
             # Link to plugin last step
             if NeoContext.__plugins[plugin_uid]['steps']['last'] is not None:
                 NeoContext.__history[NeoContext.__plugins[plugin_uid]['steps']['last']]['plugin_next'] = step_uid
                 step['plugin_previous'] = NeoContext.__history[NeoContext.__plugins[plugin_uid]['steps']['last']]['uid']
             NeoContext.__plugins[plugin_uid]['steps']['last'] = step_uid
-            
+
         # Release access
         NeoContext.__lock.release()
-        
+
         return step
-        
+
     #-----------------------------------------------------------------------------
     def createClientVar(self, name, default = None):
         # Create Vars if needed
@@ -412,7 +414,7 @@ class NeoContext():
 
             # Add property to self
             setattr(self.__class__, name, property(fget, fset))
-        
+
     #-----------------------------------------------------------------------------
     def createGlobalVar(self, name, default = None):
         # Create Vars if needed
@@ -432,7 +434,7 @@ class NeoContext():
 
             # Add property to self
             setattr(self.__class__, name, property(fget, fset))
-        
+
     #-----------------------------------------------------------------------------
     def _set_client_var(self, name, value):
         self.Vars[name] = value
@@ -440,7 +442,7 @@ class NeoContext():
     #-----------------------------------------------------------------------------
     def _get_client_var(self, name):
         return self.Vars[name]
-    
+
     #-----------------------------------------------------------------------------
     def _set_global_var(self, name, value):
         NeoContext.Vars[name] = value
@@ -448,7 +450,7 @@ class NeoContext():
     #-----------------------------------------------------------------------------
     def _get_global_var(self, name):
         return NeoContext.Vars[name]
-    
+
 
 #-----------------------------------------------------------------------------
 # NeoDialog
@@ -465,12 +467,12 @@ class NeoDialog:
 
         self.wit = Wit(self.configuration_server['wit_token'])
         self.intentscollection = self.database.intents
-        
+
         # Initialize dialogs
         self.factory = factory
         self.client = factory.clients[client_uid]
         self.client['context'] = NeoContext(factory = factory, client_uid = client_uid)
-        
+
     #-----------------------------------------------------------------------------
     def __del__(self):
         # Clean context
@@ -480,7 +482,7 @@ class NeoDialog:
             pass
         except:
             pass
-        
+
     #-----------------------------------------------------------------------------
     def parse(self, jsonData):
         # If input has already a decoded intent
@@ -493,7 +495,7 @@ class NeoDialog:
         else:
             # No input => no output
             return
-        
+
         # Initialize output from input
         jsonInput['from'], jsonInput['type'], jsonInput['zone'] = jsonData['from'], jsonData['type'], jsonData['zone']
 
@@ -501,12 +503,12 @@ class NeoDialog:
         if self.configuration_server['debug']['debug_wit']:
             log.msg("WIT: " + str(jsonInput['outcome']))
 
-        # Execute intent 
+        # Execute intent
         oIntent = self.intentscollection.find_one({"name": jsonInput['outcome']['intent']})
         if oIntent is not None:
             # Call plugin
             self.client['context'].parse(jsonInput = jsonInput, module_name = oIntent["module"], function_name = oIntent['function'])
-        else:            
+        else:
             # Parse without intent
             self.client['context'].parse(jsonInput = jsonInput)
 
